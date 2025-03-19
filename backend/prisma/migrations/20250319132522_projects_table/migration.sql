8fa99a45185567f4a54577c71fc1033dceda2264
andrelucasmartins <andrelucasmartins@hotmail.com>
2025-03-19 17:04:24 -0300

    feat(backend): db scheme done

---
+++ backend/prisma/migrations/20250319132522_projects_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "projects" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "images" TEXT[],
+    "level" INTEGER NOT NULL,
+    "type" TEXT NOT NULL,
+    "highlight" BOOLEAN NOT NULL DEFAULT false,
+    "url" TEXT NOT NULL,
+    "repository" TEXT NOT NULL,
+
+    CONSTRAINT "projects_pkey" PRIMARY KEY ("id")
+);
