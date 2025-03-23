01b5d57a98572537b424b3b9d2a68b7070ccb0d3
andrelucasmartins <andrelucasmartins@hotmail.com>
2025-03-22 23:14:00 -0300

    chore(core): remove .prettier

---
+++ core/src/project/index.ts
@@ -0,0 +1,6 @@
+import { Level } from "./Level"
+import Project from "./Project"
+import { Type } from "./Type"
+
+export { Level, Type }
+export type { Project }
---
+++ core/src/technology/Technology.ts
@@ -0,0 +1,17 @@
+export default class Technology {
+  id: number
+  name: string
+  description: string
+  image: string
+  highlight: boolean
+}
+// Render Partial properties 
+// const t: Partial<Technology> = {
+//   id: 1,
+//   name: 'React',
+//   description: 'A JavaScript library for building user interfaces',
+//   image: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+
+// }
+
+// console.log(t)
